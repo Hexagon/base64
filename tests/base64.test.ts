2b58565ceedb542e12a5e3269bdf7a7506ab28c7
Hexagon <hexagon@56k.guru>
2024-10-18 21:58:21 +0200

    Add tests. Make build pipeline work in both Deno 1 and Deno 2.

--- tests/base64.test.ts
+++ tests/base64.test.ts
@@ -1,15 +1,25 @@
 import { assertEquals } from "@std/assert";
 import { test } from "@cross/test";
 
-import { base64 } from "../src/base64.ts";
+import { base64, fromString, toString, fromArrayBuffer, toArrayBuffer, validate } from "../src/base64.ts";
 
 test("Encode 'Hello world'", function () {
   const result = base64.fromString("Hello world");
   assertEquals(result, "SGVsbG8gd29ybGQ=");
 });
 
+test("Encode 'Hello world' without going through namespace", function () {
+  const result = fromString("Hello world");
+  assertEquals(result, "SGVsbG8gd29ybGQ=");
+});
+
 test("Decode 'Hello world'", function () {
   const result = base64.toString("SGVsbG8gd29ybGQ=");
+  assertEquals(result, "Hello world");
+});
+
+test("Decode 'Hello world' without going through namespace", function () {
+  const result = toString("SGVsbG8gd29ybGQ=");
   assertEquals(result, "Hello world");
 });
 
@@ -54,6 +64,13 @@ test("Encode array buffer", function () {
   assertEquals(result, "AAECAwQFBgcI");
 });
 
+test("Encode array buffer without going through namespace", function () {
+  const result = fromArrayBuffer(
+    Uint8Array.from([0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8]).buffer,
+  );
+  assertEquals(result, "AAECAwQFBgcI");
+});
+
 test("Decode array buffer", function () {
   const result = base64.toArrayBuffer("AAECAwQFBgcI");
   assertEquals(
@@ -62,9 +79,26 @@ test("Decode array buffer", function () {
   );
 });
 
+test("Decode array buffer without going through namespace", function () {
+  const result = toArrayBuffer("AAECAwQFBgcI");
+  assertEquals(
+    result,
+    Uint8Array.from([0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8]).buffer,
+  );
+});
+
+
 // Validate
 test("AAECAwQFBgcI is valid base64 and base64url", function () {
   const resultNormal = base64.validate("AAECAwQFBgcI"),
+    resultUrl = base64.validate("AAECAwQFBgcI", true);
+
+  assertEquals(resultNormal, true);
+  assertEquals(resultUrl, true);
+});
+
+test("AAECAwQFBgcI is valid base64 and base64url withouth going through namespace", function () {
+  const resultNormal = validate("AAECAwQFBgcI"),
     resultUrl = base64.validate("AAECAwQFBgcI", true);
 
   assertEquals(resultNormal, true);
@@ -100,4 +134,14 @@ test("c3ViamVjdHM_+X2Q9MQ is neither base64 nor base64url", function () {
 
   assertEquals(resultNormal, false);
   assertEquals(resultUrl, false);
+});
+
+test("Standard tests for base64", function () {
+  assertEquals(base64.fromString(""),"");
+  assertEquals(base64.fromString("f"),"Zg==");
+  assertEquals(base64.fromString("fo"),"Zm8=");
+  assertEquals(base64.fromString("foo"),"Zm9v");
+  assertEquals(base64.fromString("foob"),"Zm9vYg==");
+  assertEquals(base64.fromString("fooba"),"Zm9vYmE=");
+  assertEquals(base64.fromString("foobar"),"Zm9vYmFy");
 });
